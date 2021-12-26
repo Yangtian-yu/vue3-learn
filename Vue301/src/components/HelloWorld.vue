0657fe7232e0417961cfef4e18e5fe697ae8f994
yangtianyu <535173883@qq.com>
2021-12-27 00:58:10 +0800

    .

---
+++ Vue301/src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <p>{{ counter }}</p>
+  <p>{{ doubleCounter }}</p>
+  <p ref="desc"></p>
+  <teleportVue></teleportVue>
+
+  <!-- emits 选项 -->
+  <Emits @click="onClick"></Emits>
+
+  <!-- vmodal -->
+  <VmodalTestVue v-model:modelValue="modelValue"></VmodalTestVue>
+</template>
+
+<script>
+import teleportVue from "./teleport.vue";
+import Emits from "./emits.vue";
+import VmodalTestVue from "./VmodalTest.vue";
+import {
+  reactive,
+  computed,
+  onUnmounted,
+  onMounted,
+  ref,
+  toRefs,
+  watch,
+} from "vue";
+export default {
+  components: {
+    teleportVue,
+    Emits,
+    VmodalTestVue,
+  },
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      modelValue: 1,
+    };
+  },
+  methods: {
+    onClick() {
+      console.log("click me!!");
+    },
+  },
+  setup() {
+    const { counter, doubleCounter } = useCounter();
+    const msg2 = ref("some message");
+    //使用元素的引用
+    const desc = ref(null);
+    //侦听器
+    watch(counter, (val, oldval) => {
+      const p = desc.value;
+      p.textContent = `counter change from ${oldval} to ${val}`;
+    });
+    return { counter, doubleCounter, msg2, desc };
+  },
+};
+
+function useCounter() {
+  const data = reactive({
+    counter: 1,
+    doubleCounter: computed(() => data.counter * 2),
+  });
+  let timer;
+  onMounted(() => {
+    timer = setInterval(() => {
+      data.counter++;
+    }, 1000);
+  });
+  onUnmounted(() => {
+    clearInterval(timer);
+  });
+  return toRefs(data);
+}
+</script>
---
+++ Vue301/src/components/emits.vue
@@ -0,0 +1,11 @@
+<template>
+  <div @click="$emit('click')">自定义时间</div>
+</template>
+
+<script>
+export default {
+  emits: ["click"],
+};
+</script>
+
+<style scoped></style>
