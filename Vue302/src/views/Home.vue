7fdac95e4e4b03af8c58fd9de27db0d11188ba99
yangtianyu <535173883@qq.com>
2022-01-02 16:19:46 +0800

    .

--- Vue302/src/views/Home.vue
+++ Vue302/src/views/Home.vue
@@ -3,12 +3,18 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <Comp :title-info="{ value: '待办事项', color: 'red' }"></Comp> -->
     <comp-setup :title-info="{ value: '待办事项', color: 'red' }"></comp-setup>
+    <div>{{ data }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Comp from "../components/Comp.vue";
 import CompSetup from "@/components/CompSetup.vue";
+import dayjs from "dayjs";
+import { ref } from "vue";
+
+const data = ref(
+  dayjs(Number(new Date()) + 3600 * 8 * 1000).format("YYYY-MM-DD HH:mm:ss")
+);
 </script>
 
 <style scoped></style>
