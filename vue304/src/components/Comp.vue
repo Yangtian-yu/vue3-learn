2f31ed3dcc75e89560251c9e3a1b8012240f18c7
yangtianyu <535173883@qq.com>
2022-01-04 01:35:44 +0800

    .

---
+++ vue304/src/components/Comp.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  mounted() {
+    this;
+  },
+  created() {
+    this;
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
