c4b1f4c9c5a5d7b6410eedbe96989ec6a03c8a44
yangtianyu <535173883@qq.com>
2022-01-16 19:09:13 +0800

    .

---
+++ geek-admin/src/components/Count.vue
@@ -0,0 +1,15 @@
+<template>
+  <div @click="addCount">{{ count }}</div>
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { computed } from "vue";
+const store = useStore();
+let count = computed(() => store.state.count);
+const addCount = () => {
+  store.commit("addCount");
+};
+</script>
+
+<style scoped></style>
--- geek-admin/src/pages/Home.vue
+++ geek-admin/src/pages/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <h1>这是首页</h1>
+  <Count></Count>
+  <br />
   <Todolist></Todolist>
 </template>
 <script setup>
 import Todolist from "../components/Todolist.vue";
+import Count from "../components/Count.vue";
 </script>
 <style scoped></style>
